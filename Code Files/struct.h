b815908e2b38db980f29931e443db15cabe1c97b
Ishrak Adit <ishrak.adit07@gmail.com>
2024-05-02 07:10:15 +0600

    Code Files Commit

---
+++ Code Files/struct.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+
+typedef struct whiskers 
+{
+	float xmove;
+	int ymove;
+	float speed;
+}whisker;
+
+
+typedef struct object
+{
+	int xstart;
+	int xmove;
+	int speed;
+}obj;
